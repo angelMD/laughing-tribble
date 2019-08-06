1e62ea17becd33390f8725c79a0b8a08e54590d8
Ryan Stephens <rlstephens@gmail.com>
2019-08-06 14:27:13 -0600

    roll call

--- ruby/spec/3_spec.rb
+++ ruby/spec/3_spec.rb
@@ -1,40 +1,22 @@
-class RollCallBooleanError < StandardError
-  def message
-    'Status needs to be either true or false'
-  end
-end
-
-class RollCallPercentError < StandardError
-  def message
-    'Grouping needs to be either present or absent'
-  end
-end
-
 class RollCall
-  attr_reader :roll
-
-  def initialize
-    @string_date = Date.today.to_s
-    @roll = roll_builder
-    @q = @roll[@string_date]
+  def today
+    @today ||= Date.today.to_s
   end
 
-  def roll_builder
-    {
-      @string_date => {
+  def roll
+    @roll ||= {
+      today => {
         :present => [],
         :absent => []
       }
     }
   end
 
-  def student_check(name:, status:)
-    raise RollCallBooleanError unless (status == true || status == false)
-
-    if status
-      @q[:present].push(name)
+  def student_checkin(name, present)
+    if present
+      roll[today][:present].push(name)
     else
-      @q[:absent].push(name)
+      roll[today][:absent].push(name)
     end
   end
 
@@ -45,25 +27,23 @@ class RollCall
   end
 
   def roll_percentage(grouping)
-    total_students = @q.values.flatten.count
-    present_percent = (@q[:present].count.to_f / total_students.to_f) * 100.0
+    total_students = roll[today].values.flatten.count
+    present_percent = (roll[today][:present].count.to_f / total_students.to_f) * 100.0
 
     if grouping == 'present'
       present_percent
     elsif grouping == 'absent'
       100 - present_percent
-    else
-      raise RollCallPercentError
     end
   end
 
   def formatted_roll
     <<~ROLLDOC
-      ## Roll call for #{@string_date}
+      ## Roll call for #{today}
       ### #{roll_percentage('present')}% Present
-      #{@q[:present].join(', ')}
+      #{roll[today][:present].join(', ')}
       ### #{roll_percentage('absent')}% Absent
-      #{@q[:absent].join(', ')}
+      #{roll[today][:absent].join(', ')}
       #{'-' * 42}
     ROLLDOC
   end
@@ -72,27 +52,27 @@ end
 describe RollCall do
   before do
     @rc = RollCall.new
-    @rc.student_check(name: 'Ryan', status: true)
-    @rc.student_check(name: 'Chase', status: true)
-    @rc.student_check(name: 'KJ', status: true)
-    @rc.student_check(name: 'Hunter', status: false)
-    @rc.student_check(name: 'Devan', status: false)
-    @rc.student_check(name: 'Zach', status: true)
-    @rc.student_check(name: 'Ben', status: true)
-    @rc.student_check(name: 'Stephanie', status: true)
-    @rc.student_check(name: 'Ian', status: true)
-    @rc.student_check(name: 'Alex', status: true)
-    @rc.student_check(name: 'Cody', status: true)
-    @rc.student_check(name: 'Preston', status: true)
-    @rc.student_check(name: 'Andrew', status: true)
-    @rc.student_check(name: 'Jason', status: true)
-    @rc.student_check(name: 'Christian', status: false)
+    @rc.student_checkin('Ryan', true)
+    @rc.student_checkin('Chase', true)
+    @rc.student_checkin('KJ', true)
+    @rc.student_checkin('Hunter', false)
+    @rc.student_checkin('Devan', false)
+    @rc.student_checkin('Zach', true)
+    @rc.student_checkin('Ben', true)
+    @rc.student_checkin('Stephanie', true)
+    @rc.student_checkin('Ian', true)
+    @rc.student_checkin('Alex', true)
+    @rc.student_checkin('Cody', true)
+    @rc.student_checkin('Preston', true)
+    @rc.student_checkin('Andrew', true)
+    @rc.student_checkin('Jason', true)
+    @rc.student_checkin('Christian', false)
   end
 
   it 'can take roll for students and return a hash that lists the students that were present and absent' do
     expect(@rc.roll).to eq(
       {
-        "2017-04-12"=>
+        "2018-08-06"=>
         {
           :present=>[
             "Ryan",
@@ -123,11 +103,9 @@ describe RollCall do
   end
 
   it 'appends the daily roll call to a file' do
-    # In order for this test you to pass, you will need to have a directory
-    # named 'support' on the path that you call the RSpec test from
     @rc.save_to_file
     content = <<~CONTENT
-      ## Roll call for 2017-04-12
+      ## Roll call for 2018-08-06
       ### 80.0% Present
       Ryan, Chase, KJ, Zach, Ben, Stephanie, Ian, Alex, Cody, Preston, Andrew, Jason
       ### 20.0% Absent
